00a7622824b3f26ae50512edc2b506c7e7cd2716
Soha0089 <habir4746@gmail.com>
2025-07-30 03:45:31 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  name: { type: String, default: "" }, // ✅ নামের ডিফল্ট
+  id: { type: String, required: true, unique: true }, // WhatsApp ID
+  name: { type: String, default: "" },                // User's display name
   coins: { type: Number, default: 0 },
   exp: { type: Number, default: 0 },
   level: { type: Number, default: 1 },
